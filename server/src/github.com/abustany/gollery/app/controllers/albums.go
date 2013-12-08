468fd16ece805526d83d8f9404f16141a6830939
Adrien Bustany <adrien@bustany.org>
2013-12-09 00:29:30 +0100

    albums: Include the EXIF metadata in album listings

--- server/src/github.com/abustany/gollery/app/controllers/albums.go
+++ server/src/github.com/abustany/gollery/app/controllers/albums.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/abustany/goexiv"
 	"github.com/abustany/gollery/app"
 	"github.com/abustany/gollery/thumbnailer"
 	"github.com/abustany/gollery/utils"
@@ -26,6 +27,32 @@ type AlbumData struct {
 
 type PictureInfo struct {
 	Path string `json:"path"`
+	Metadata map[string]string `json:"metadata"`
+}
+
+var ExifKeys = []string{
+	"Exif.Photo.ExifVersion",
+	"Exif.Image.Make",
+	"Exif.Image.Model",
+	"Exif.Photo.DateTimeOriginal",
+	"Exif.Photo.ExposureTime",
+	"Exif.Photo.ShutterSpeedValue",
+	"Exif.Photo.FNumber",
+	"Exif.Photo.ApertureValue",
+	"Exif.Photo.ExposureBiasValue",
+	"Exif.Photo.Flash",
+	"Exif.Photo.FocalLength",
+	"Exif.Photo.FocalLengthIn35mmFilm",
+	"Exif.Photo.SubjectDistance",
+	"Exif.Photo.ISOSpeedRatings",
+	"Exif.Photo.ExposureProgram",
+	"Exif.Photo.MeteringMode",
+	"Exif.Image.ImageWidth",
+	"Exif.Photo.PixelXDimension",
+	"Exif.Image.ImageLength",
+	"Exif.Photo.PixelYDimension",
+	"Exif.Image.Copyright",
+	"Exif.Photo.UserComment",
 }
 
 func (c *Albums) Index() revel.Result {
@@ -46,6 +73,40 @@ func (c *Albums) Index() revel.Result {
 	c.Response.Status = http.StatusOK
 	c.Response.ContentType = "application/json"
 	return c.RenderJson(dirs)
+}
+
+func getMetadata(filePath string) (map[string]string, error) {
+	fd, err := goexiv.Open(filePath)
+
+	if err != nil {
+		return nil, utils.WrapError(err, "Cannot open file to read metadata")
+	}
+
+	err = fd.ReadMetadata()
+
+	if err != nil {
+		return nil, utils.WrapError(err, "Cannot read metadata")
+	}
+
+	data := fd.GetExifData()
+
+	metadata := make(map[string]string, len(ExifKeys))
+
+	for _, key := range ExifKeys {
+		val, err := data.FindKey(key)
+
+		if err != nil {
+			return nil, utils.WrapError(err, "Invalid EXIF key requested")
+		}
+
+		if val == nil {
+			continue
+		}
+
+		metadata[key] = val.String()
+	}
+
+	return metadata, nil
 }
 
 func (c *Albums) Show(name string) revel.Result {
@@ -91,8 +152,16 @@ func (c *Albums) Show(name string) revel.Result {
 			continue
 		}
 
+		metadata, err := getMetadata(filePath)
+
+		if err != nil {
+			revel.ERROR.Printf("Cannot read metadata for file '%s': %s", filePath, err)
+			metadata = map[string]string{}
+		}
+
 		picInfo := &PictureInfo{
 			Path: info.Name(),
+			Metadata: metadata,
 		}
 
 		albumData.Pictures = append(albumData.Pictures, picInfo)
