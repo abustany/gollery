46645c8cd843356b5736e63f57899cb2a485fb00
Adrien Bustany <adrien@bustany.org>
2014-05-21 00:32:35 +0200

    Add a Grunt 'package' command
    This produces a tar archive that can be deployed on a server without Go
    installed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,6 +50,44 @@ module.exports = function (grunt) {
 				files: ['src/gollery/static/typescript/*.ts'],
 				tasks: ['typescript']
 			}
+		},
+		shell: {
+			package: {
+				command: function () {
+					var filesToKeep = [
+						'gollery',
+						'run.sh',
+						'src/gollery/app/views',
+						'src/gollery/conf/app.conf.dist',
+						'src/gollery/conf/routes',
+						'src/gollery/static/i18n',
+						'src/gollery/static/images',
+						'src/gollery/static/index.html',
+						'src/gollery/static/index.js',
+						'src/gollery/static/index.js.map',
+						'src/gollery/static/js/requirejs-?.?.?.js',
+						'src/gollery/static/stylesheets',
+						'src/github.com/robfig/revel/conf/mime-types.conf',
+						'src/github.com/robfig/revel/modules/jobs/conf/routes',
+						'src/github.com/robfig/revel/modules/testrunner/conf/routes',
+						'src/github.com/robfig/revel/templates'
+					];
+
+					return [
+						'unset GOPATH',
+						'export GOLLERY_INSTALL_REVEL=1',
+						'export GOLLERY_VERSION=$(git describe --always --dirty)',
+						'source ./env.sh',
+						'go get gollery/...',
+						'./bin/revel package gollery',
+						'mkdir gollery-$GOLLERY_VERSION',
+						'tar -C gollery-$GOLLERY_VERSION -xf gollery.tar.gz -- ' + filesToKeep.join(' '),
+						'tar cjf gollery-$GOLLERY_VERSION.tar.bz2 gollery-$GOLLERY_VERSION',
+						'rm -fr gollery-$GOLLERY_VERSION gollery.tar.gz', // Bundles the whole source folder - not needed
+						'echo -e "\\nFinal gollery archive ready in gollery-$GOLLERY_VERSION.tar.bz2"'
+					].join('&&');
+				}
+			}
 		}
 	});
 
@@ -57,7 +95,9 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
 	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['compass', 'uglify', 'typescript', 'requirejs']);
+	grunt.registerTask('package', ['default', 'shell:package']);
 };
